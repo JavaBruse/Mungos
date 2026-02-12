e39961794a853fe2ab46fa1100b65446c295dabc
FiataliS <FiataliS@gmail.com>
2026-02-12 15:45:22 +0500

    add docker file and compose from all module project

--- core/src/test/java/com/JavaBruse/core/CoreApplicationTests.java
+++ core/src/test/java/com/JavaBruse/core/CoreApplicationTests.java
@@ -1,13 +1,8 @@
 package com.JavaBruse.core;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
